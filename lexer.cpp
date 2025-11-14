cef5a6074f4a851a59555477bffba6f4054728be
Javier-H3 <hernandezgomezjavier369@gmail.com>
2025-11-13 20:01:38 -0500

    my first commit and creating the file lexer.cpp-firt version

---
+++ lexer.cpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+
+enum class TokenType {
+    id,
+    INT,
+    FLOAT,
+    sum, res, mul, div,
+    assing,
+    Lparen, Rparen,
+    semiColon,
+    End_of_line,
+    unknown
+};
+
+struct Token
+{
+    TokenType type;
+    std::string lexeme;
+    int line;
+    int col;
+};
+
+class lexer
+{
+private:
+    std::string input;
+    size_t pos;
+    int line;
+    int col;
+
+    char peek() const;
+    char get() const;
+    void skipWhiteSpace();
+    Token identifier();
+    Token number();    
+    
+public:
+     explicit lexer(const std::string& input);
+     Token genNextToken();
+};
+
+
+
