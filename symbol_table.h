c695128cf5e547a1fcd202e800cbb5feabbab4fb
Javier-H3 <hernandezgomezjavier369@gmail.com>
2025-11-26 15:06:36 -0500

    add type_Table.h and and the .cpp files were updated to .h (headers)

---
+++ symbol_table.h
@@ -0,0 +1,48 @@
+//symbol_table.h
+#pragma once
+#include <string>
+#include <unordered_map>
+#include <iostream>
+#include "ast.h"
+
+struct SymbolInfo {
+    std::string name;
+    ValueType type;
+};
+
+class symbol_table {
+
+    private:
+        std::unordered_map<std::string, SymbolInfo> table;
+
+        std::string valueTypeToString(ValueType t) const {
+            switch (t) {
+            case ValueType::type_int: return "int";
+            case ValueType::type_float: return "float";
+            default: return "error";
+            }
+        }
+
+    public:
+        bool insert(const std::string& name, ValueType type){
+            if (table.find(name) != table.end()) return false;
+            table[name] = SymbolInfo{name, type};
+            return true;
+        }
+
+        SymbolInfo* lookup(const std::string& name) {
+            auto it = table.find(name);
+            if (it == table.end()) return nullptr;
+            return &it->second;
+        }
+
+        void print() const {
+            std::cout << "===== TABLA DE SÍMBOLOS =====\n";
+            std::cout << "Nombre\tTipo\n";
+            std::cout << "-----------------------------\n";
+                for (const auto& [k, sym] : table) {
+                    std::cout << sym.name << "\t" << valueTypeToString(sym.type) << "\n";
+                }
+            std::cout << "=============================\n";
+    }
+};
---
+++ type_table.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "ast.h"
+#include <array>
+#include <iostream>
+
+class TypeTable
+{
+private:
+    std::array<std::array<ValueType, 3>, 3> addTable;
+    std::array<std::array<ValueType, 3>, 3> mulTable;
+
+    void printTable(const std::array<std::array<ValueType,3>,3>& table) const {
+        std::string names[3] = {"int", "float", "error"};
+        std::cout<<"\t";
+
+        for(int j = 0; j < 3; j++) std::cout<<names[j]<<"\t";
+        
+        std::cout<<"\n";
+
+        for (int i = 0; i < 3; i++) {
+            std::cout<<names[i]<<"\t";
+            for (int j = 0; j < 3; j++) {
+                std::cout<<names[(int)table[i][j]]<<"\t";
+            }
+            std::cout<<"\n";
+        }       
+    }
+
+public:
+    TypeTable () {
+        for (auto &row : addTable) row.fill(ValueType::type_error);
+        for (auto &row : mulTable) row.fill(ValueType::type_error);
+
+        addTable[(int)ValueType::type_int][(int)ValueType::type_int]     = ValueType::type_int;
+        addTable[(int)ValueType::type_int][(int)ValueType::type_float]   = ValueType::type_float;
+        addTable[(int)ValueType::type_float][(int)ValueType::type_int]   = ValueType::type_float;
+        addTable[(int)ValueType::type_float][(int)ValueType::type_float] = ValueType::type_int;
+
+        mulTable = addTable;
+    }
+
+    ValueType resultTypeFor(const std::string& op, ValueType left, ValueType right) {
+        if (op == "+" || op == "-")
+          return addTable[(int)left][(int)right];
+        if (op == "*" || op == "/")
+            return mulTable[(int)left][(int)right];
+          return ValueType::type_error; 
+    }
+
+    void print() const {
+        std::cout << "===== TABLA DE TIPOS (Suma/Resta) =====\n";
+        printTable(addTable);
+        std::cout << "===== TABLA DE TIPOS (Mult/Div) =====\n";
+        printTable(mulTable);
+    }
+};
